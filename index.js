519cd5401f5756473e9d738d0fb70f538851db99
Farzin Firoozi <farzin7813@gmail.com>
2021-02-28 11:29:52 +0330

    linux version

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const Shell = require("node-powershell");
+// const Shell = require("node-powershell");
+const { exec } = require("child_process");
+
 const Downloader = require("nodejs-file-downloader");
-const ps = new Shell({
-  executionPolicy: "Bypass",
-  noProfile: true,
-});
+// const ps = new Shell({
+//   executionPolicy: "Bypass",
+//   noProfile: true,
+// });
 
 const dl = async (url, directory, name) => {
   console.log("downloading", directory, name);
@@ -32,52 +34,30 @@ const downloadMovie = async (session_id, file_name) => {
   url = `https://meeting.kashanu.ac.ir/presentation/${session_id}/video/webcams.mp4`;
   await dl(url, file_name, "audio.mp4");
 
-  try {
-    ps.addCommand(
-      `ffmpeg -i ${file_name}/screen.mp4 -i ${file_name}/audio.mp4 -c copy -map 0:0 -map 1:1  out/${file_name}.mp4 `
-    );
-    await ps.invoke();
-  } catch (error) {
-    console.warn("ffmpeg was Diverted");
-  }
-  await ps.dispose();
+  exec(
+    `ffmpeg -i ${file_name}/screen.mp4 -i ${file_name}/audio.mp4 -c copy -map 0:0 -map 1:1  out/${file_name}.mp4 `,
+    (err, stdout, stderr) => {
+      if (err) {
+        //some err occurred
+        console.error(err);
+      } else {
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      }
+    }
+  );
+
+  // try {
+  //   ps.addCommand();
+  //   await ps.invoke();
+  // } catch (error) {
+  //   console.warn("ffmpeg was Diverted");
+  // }
+  // await ps.dispose();
 };
 
-const data = [
-  {
-    name: "99.09.18",
-    id: "976848b6f5623bc884acaae59ecc4fc81d6d9bc9-1607428240345",
-  },
-  {
-    name: "99.09.25",
-    id: "976848b6f5623bc884acaae59ecc4fc81d6d9bc9-1608033234237",
-  },
-  {
-    name: "99.09.30",
-    id: "976848b6f5623bc884acaae59ecc4fc81d6d9bc9-1608465291119",
-  },
-  {
-    name: "99.10.02",
-    id: "976848b6f5623bc884acaae59ecc4fc81d6d9bc9-1608638144117",
-  },
-  {
-    name: "99.10.07",
-    id: "976848b6f5623bc884acaae59ecc4fc81d6d9bc9-1609070038114",
-  },
-  {
-    name: "99.10.09",
-    id: "976848b6f5623bc884acaae59ecc4fc81d6d9bc9-1609242390426",
-  },
-  {
-    name: "99.10.14",
-    id: "976848b6f5623bc884acaae59ecc4fc81d6d9bc9-1609674563803",
-  },
-  {
-    name: "99.10.16",
-    id: "976848b6f5623bc884acaae59ecc4fc81d6d9bc9-1609847732613",
-  },
-];
-
-let i = 7;
-
-downloadMovie(data[i].id, data[i].name);
+downloadMovie(
+  "74f611125da15b6da6c449caa909ee71533d458c-1614060029439",
+  "compiler_3"
+);
